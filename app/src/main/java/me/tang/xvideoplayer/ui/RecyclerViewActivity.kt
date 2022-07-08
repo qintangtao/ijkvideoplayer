0b7c70b92c51f0aae7cb1c21d6b1598833a73ee5
Tangtao Qin <qintangtao@qq.com>
2022-07-08 15:22:13 +0800

    setRecyclerListener

--- app/src/main/java/me/tang/xvideoplayer/ui/RecyclerViewActivity.kt
+++ app/src/main/java/me/tang/xvideoplayer/ui/RecyclerViewActivity.kt
@@ -2,6 +2,9 @@ package me.tang.xvideoplayer.ui
 
 import android.os.Bundle
 import me.tang.mvvm.base.BaseActivity
+import me.tang.xvideoplayer.R
+import me.tang.xvideoplayer.XLog
+import me.tang.xvideoplayer.XVideoPlayer
 import me.tang.xvideoplayer.databinding.ActivityRecyclerViewBinding
 
 class RecyclerViewActivity : BaseActivity<RecyclerViewViewModel, ActivityRecyclerViewBinding>() {
@@ -9,6 +12,12 @@ class RecyclerViewActivity : BaseActivity<RecyclerViewViewModel, ActivityRecycle
 
     override fun initView(savedInstanceState: Bundle?) {
         mBinding.viewModel = viewModel
+
+        mBinding.recyclerView.setRecyclerListener {
+            XLog.d("setRecyclerListener -> ${it.itemView}")
+            val videoPlayer = it.itemView.findViewById<XVideoPlayer>(R.id.videoPlayer)
+            videoPlayer?.release()
+        }
     }
 
     override fun initData() {
